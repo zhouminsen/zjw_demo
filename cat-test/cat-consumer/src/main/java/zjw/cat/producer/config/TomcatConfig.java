ab2f622df01f836dea01e90eb0feed68391e0b43
zhoujiawei <992427>
2020-07-16 13:17:44 +0800

    get请求支持特殊字符串

---
+++ cat-test/cat-consumer/src/main/java/zjw/cat/producer/config/TomcatConfig.java
@@ -0,0 +1,22 @@
+package zjw.cat.producer.config;
+
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TomcatConfig {
+
+    /**
+     * get请求支持特殊字符串
+     * @return
+     */
+    @Bean
+    public ServletWebServerFactory webServerFactory() {
+        TomcatServletWebServerFactory fa = new TomcatServletWebServerFactory();
+        fa.addConnectorCustomizers((TomcatConnectorCustomizer) connector -> connector.setProperty("relaxedQueryChars", "[]{}"));
+        return fa;
+    }
+}
