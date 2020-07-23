66799e3ef3be0f915f47901aedc5dd3ccbabb367
zhouminsen@126.com <weijia1992427>
2020-07-23 22:32:43 +0800

    条形扫码前后端通过

--- barcode-scan/src/main/java/com/cjlogistics/barcode/controller/ScanController.java
+++ barcode-scan/src/main/java/com/cjlogistics/barcode/controller/ScanController.java
@@ -1,21 +1,37 @@
 package com.cjlogistics.barcode.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Administrator on 2020-05-20.
  */
-@RequestMapping("/student")
-@RestController
+@RequestMapping("")
+@Controller
 public class ScanController {
 
 
-    @GetMapping("/helloWorld")
-    public Object helloWorld(@RequestParam String code) {
+    @GetMapping("")
+    public String index() {
 
-        return "haha";
+        return "index";
+    }
+
+    @GetMapping("/getData")
+    @ResponseBody
+    public Map<String, Object> getData(@RequestParam String code) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("id", 1);
+        map.put("id2", 2);
+        map.put("id3", 3);
+        map.put("id4", 4);
+        map.put("id5", code);
+        return map;
     }
 }
---
+++ barcode-scan/src/main/java/com/cjlogistics/barcode/controller/TomcatConfig.java
@@ -0,0 +1,18 @@
+package com.cjlogistics.barcode.controller;
+
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TomcatConfig {
+
+    @Bean
+    public ServletWebServerFactory webServerFactory() {
+        TomcatServletWebServerFactory fa = new TomcatServletWebServerFactory();
+        fa.addConnectorCustomizers((TomcatConnectorCustomizer) connector -> connector.setProperty("relaxedQueryChars", "[]{}"));
+        return fa;
+    }
+}
